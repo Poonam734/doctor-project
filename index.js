eecda52f7ad3b8cba3b5cf16df97b6978b8e734e
Poonam734 <panchariyapoonam2@gmail.com>
2024-06-21 17:49:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(patientRoutes)
 
 
 
-mongoose.connect("mongodb+srv://nitin07singhal:dCFOl2TRnlMLUFhb@workshop.uktxcbm.mongodb.net/doctorsapp?retryWrites=true&w=majority&appName=workshop").then(()=>{
+mongoose.connect("mongodb+srv://panchariya006p:TR97xRpwvL91K1fm@cluster0.rz8t3sb.mongodb.net/doctor-project?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("connected to mongoDB");
 }).catch((err)=>{
     console.log(err);
